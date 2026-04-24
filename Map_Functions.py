22fc372b8add24d2fd1b2f10972635f92acb5e6a
Meet Sheth <meetsheth2710@gmail.com>
2026-04-24 12:26:28 +0530

    updated Map_Functions

--- Map_Functions.py
+++ Map_Functions.py
@@ -6,3 +6,33 @@ def double(n):
 n = [5, 6, 7, 8]
 res = map(double, n)
 print(list(res))
+
+
+# Reduce  function
+import functools
+
+n = [1, 2, 3, 4]
+
+prod = functools.reduce(lambda x, y: x * y, n)
+print(prod)
+
+# Filter Function
+
+def is_even(n):
+    return n % 2 == 0
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+en = filter(is_even, n)
+print(list(en))
+
+# Combined Examples
+from functools import reduce
+
+n = [1, 2, 3, 4, 5, 6]
+
+e = filter(lambda x: x % 2 == 0, n)
+s = map(lambda x: x**2, e)
+res = reduce(lambda x, y: x + y, s)
+
+print(res)
