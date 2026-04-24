5a75d6efb4a13e5b718c468b9438b43c52f6ed92
Meet Sheth <meetsheth2710@gmail.com>
2026-04-24 13:03:21 +0530

    Updated  new file

--- Lambda.py
+++ Lambda.py
@@ -1,3 +1,7 @@
+# Lambda functions is done in one line and has no return statement in them.
+
+# Lambda function to check if a number is positive, negative, or zero
+
 check = lambda x: "Positive" if x > 0 else "Negative" if x < 0 else "Zero"
 print(check(5))   
 print(check(-3))  
